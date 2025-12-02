886b7e0a9f921a2c4a121ba3613d80ddc1ddc626
suryatejagajjela <gajjelasuryateja2021@gmail.com>
2025-12-02 15:12:15 -0500

    Enable global CORS for Actuator endpoints

--- crypto-alerting-ingestion/src/main/java/com/crypto/alerting/ingestion/IngestionServiceApplication.java
+++ crypto-alerting-ingestion/src/main/java/com/crypto/alerting/ingestion/IngestionServiceApplication.java
@@ -3,9 +3,26 @@ package com.crypto.alerting.ingestion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
 @SpringBootApplication
 public class IngestionServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(IngestionServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebFluxConfigurer corsConfigurer() {
+        return new WebFluxConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
