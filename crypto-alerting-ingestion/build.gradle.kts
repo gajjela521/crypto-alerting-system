280a1345c90adfd6e494951b139fc7adaf694e0d
suryatejagajjela <gajjelasuryateja2021@gmail.com>
2025-12-02 14:04:48 -0500

    Add AssertJ core test dependency

--- crypto-alerting-ingestion/build.gradle.kts
+++ crypto-alerting-ingestion/build.gradle.kts
@@ -7,5 +7,25 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("io.projectreactor.kafka:reactor-kafka")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.3.0")
     implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
+    implementation("io.github.resilience4j:resilience4j-reactor")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
+    testImplementation("net.bytebuddy:byte-buddy:1.14.19")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+    testImplementation("org.apache.kafka:kafka_2.13")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.0")
+    }
 }
